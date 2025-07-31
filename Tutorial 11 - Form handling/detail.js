8e64a5d4acc2bda5f567a8baf5ca36dd2f354c89
ibad-hussain <ibadhussain2212@gmail.com>
2025-07-31 20:17:05 +0500

    Added Tutorial 11

---
+++ Tutorial 11 - Form handling/detail.js
@@ -0,0 +1,10 @@
+// react-hook-form npm package
+
+// Basic React form handling
+    // Create form
+    // Register fields
+    // Handle submit
+    // Validation
+    // Error handling
+    // Styling errors
+    // Multiple submission prevention
